508f1b764266f03a9d5d80694e6de4b226d0181f
ghofranaaa <smailikhaled841@gmail.com>
2024-07-26 19:05:02 +0100

    specific string

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,10 @@
+export default function cleanSet(set, startString) {
+  if (!startString || typeof startString !== 'string') return '';
+  const result = [];
+    set.forEach((value) => {
+  if (value.startsWith(startString)) {
+    result.push(value.substring(startString.length));
+    }
+  });
+  return result.join('-');
+}
