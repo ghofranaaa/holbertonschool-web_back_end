0315e2f6bd1dd7b97d10791091b9636fb1ad8035
ghofranaaa <smailikhaled841@gmail.com>
2024-07-27 17:03:23 +0100

    stat and value

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,16 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const user = await signUpUser(firstName, lastName);
+  let photo = null;
+  try {
+    photo = await uploadPhoto(fileName);
+  }
+    catch (err) {
+    photo = `${err.name}: ${err.message}`;
+  }
+  const promise1 = { status: 'fulfilled', value: user };
+  const promise2 = { status: 'rejected', value: photo };
+  return [promise1, promise2];
+}
