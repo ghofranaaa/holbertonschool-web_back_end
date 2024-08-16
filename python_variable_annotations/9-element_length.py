5c2416a41cac326467cc80335cf8d8ba4375b142
ghofranaaa <smailikhaled841@gmail.com>
2024-08-16 22:05:58 +0100

    concat

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to calculate the
+length of elements in an iterable.
+"""
+from typing import Iterable, Sequence, List, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List [Tuple[Sequence, int]]:
+    """
+    Returns a list of tuples where each tuple contains an element
+    from the iterable and its length.
+    """
+    return [(i, lem(i)) for i in lst]
