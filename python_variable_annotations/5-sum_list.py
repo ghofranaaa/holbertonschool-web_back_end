9a6e75fa846ddc7422ed7d7f97bd4d44c3e96a46
ghofranaaa <smailikhaled841@gmail.com>
2024-08-16 21:41:59 +0100

    concat

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to calculate the sum of a list of floats.
+"""
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Returns the sum of a list of floats."""
+    return sum(input_list)
