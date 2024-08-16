4e9bb1935c1a0f43f610e9eb4510c03792fdb677
ghofranaaa <smailikhaled841@gmail.com>
2024-08-16 21:37:47 +0100

    concat

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-This module defines and annotates several variables with specific types and values.
+This module defines and annotates several variables with specific
+types and values.
 """
 
 
