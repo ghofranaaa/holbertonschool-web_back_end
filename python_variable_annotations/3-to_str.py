908c0ba24a21842f807c3329e4e770ff6961d17d
ghofranaaa <smailikhaled841@gmail.com>
2024-08-16 21:31:13 +0100

    concat

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+This module provides a simple function returns the string representation of the float.
+"""
+
+
+def to_str(n: float) -> str:
+    """Returns the string representation of the float."""
+    return str(n)
