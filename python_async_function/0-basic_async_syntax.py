df6e69e63df6dcfc37d74a7e9164cb58efba1cd0
ghofranaaa <smailikhaled841@gmail.com>
2024-08-17 19:26:46 +0100

    finished

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python3
 """
-This module provides an asynchronous coroutine
-`wait_random` that waits for a random delay 
-and returns the delay time.
+This module provides an asynchronous coroutine `wait_random`
+that waits for a random delay and returns the delay time.
 """
-
-import asyncio
 import random
-
+import asyncio
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """Asynchronous coroutine that waits for a random
-    delay and returns it."""
+    """Asynchronous coroutine that waits for a
+    random delay and returns it."""
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
