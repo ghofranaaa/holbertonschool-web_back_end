1022dbb11ab60a3c57b8054f11ef757072aa9305
ghofranaaa <smailikhaled841@gmail.com>
2024-08-17 19:31:10 +0100

    finished

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 """
 This module provides an asynchronous coroutine `wait_n`
-that spawns multiple `wait_random` tasks and returns their sorted delays.
+that spawns multiple `wait_random` tasks and returns
+their sorted delays.
 """
 import asyncio
 from typing import List
-from 0-basic_async_syntax import wait_random
+
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Spawns wait_random n times with the specified max_delay."""
-    delays = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
-    return sorted(delay)
+    delays = await asyncio.gather(*[wait_random(max_delay) for _ in range(n)])
+    return sorted(delays)
