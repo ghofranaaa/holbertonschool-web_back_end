8092c40218eb10af64c49752baf1d4e7c8a8d78b
ghofranaaa <smailikhaled841@gmail.com>
2024-08-21 21:53:55 +0100

    async generator

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,23 +1,28 @@
 #!/usr/bin/env python3
 """
-This module provides a coroutine function to measure the
-execution time of running multiple asynchronous comprehensions in parallel.
-"""
+This module measures the runtime of executing async comprehensions \
+in parallel.
 
-import time
+Functions:
+    measure_runtime: Measures the total runtime of running \
+    async_comprehension four times in parallel.
+"""
 import asyncio
-async_comprehension = __import__('1-async_comprehension').async_comprehension
+import time
+async_comprehension = __import__("1-async_comprehension").async_comprehension
 
 
 async def measure_runtime() -> float:
     """
-    Executes async_comprehension four times in parallel and measures
+    Executes async_comprehension four times in parallel and measures \
     the total runtime.
 
     Returns:
-        float: The total runtime in seconds.
+        float: The total runtime taken to execute the four \
+        async comprehensions.
     """
     start_time = time.time()
-    await asyncio.gather(*[async_comprehension() for _ in range(4)])
+    await asyncio.gather(async_comprehension())
     end_time = time.time()
-    return end_time - start_time
+    run_time = end_time - start_time
+    return run_time
