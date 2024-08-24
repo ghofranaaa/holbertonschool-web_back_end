56a9a14252472bb99386f7a0db73bc1767738b9d
ghofranaaa <smailikhaled841@gmail.com>
2024-08-24 12:50:47 +0100

    pagination

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -43,7 +43,7 @@ class Server:
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Any]:
         """Return a dictionary containing hypermedia pagination details."""
         data = self.get_page(page, page_size)
-        total_pages = math.ceil(len(self.dataset())/ page_size)
+        total_pages = math.ceil(len(self.dataset()) / page_size)
 
         next_page = page + 1 if page < total_pages else None
         prev_page = page - 1 if page > 1 else None
