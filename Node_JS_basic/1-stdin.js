6637fdad829fb2ebe8c348f819b51eba7a81d696
ghofranaaa <smailikhaled841@gmail.com>
2024-09-21 19:04:42 +0100

    corrected

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,10 +1,9 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\r');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
-	const name = data.toString().trim();
-	process.stdout.write(`Your name is: ${name}\r`); // Use \r here
+  process.stdout.write(`Your name is: ${data}`);
 });
 
 process.stdin.on('end', () => {
-	process.stdout.write('This important software is now closing\r'); // Corrected typo and use \r
+  process.stdout.write('This important software is now closing\n');
 });
