fd3af55d8d101bb5440e7c3500ef7b0eaac7bfbf
ghofranaaa <smailikhaled841@gmail.com>
2024-09-21 19:14:22 +0100

    corrected

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -3,19 +3,19 @@ const countStudents = require('./3-read_file_async');
 
 const app = http.createServer((req, res) => {
     if (req.url === '/') {
-        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.end('Hello Holberton School!');
     } else if (req.url === '/students') {
         let responseText = 'This is the list of our students\n';
 
         countStudents(process.argv[2])
-            .then((data) => {
-                responseText += data;
-                res.writeHead(200, {'Content-Type': 'text/plain'});
+            .then((studentInfo) => {
+                responseText += studentInfo;
+                res.writeHead(200, { 'Content-Type': 'text/plain' });
                 res.end(responseText);
             })
             .catch(() => {
-                res.writeHead(500, {'Content-Type': 'text/plain'});
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
                 res.end('Cannot load the database');
             });
     }
