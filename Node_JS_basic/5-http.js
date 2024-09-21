63388732231335fde77a9d303f83681de8ed4589
ghofranaaa <smailikhaled841@gmail.com>
2024-09-21 19:32:32 +0100

    corrected

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -2,24 +2,24 @@ const http = require('http');
 const countStudents = require('./3-read_file_async');
 
 const app = http.createServer((req, res) => {
-    if (req.url === '/') {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('Hello Holberton School!');
-    } else if (req.url === '/students') {
-        let responseText = 'This is the list of our students\n';
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    let responseText = 'This is the list of our students\n';
 
-        countStudents(process.argv[2])
-            .then((studentInfo) => {
-                responseText += studentInfo;
-                res.writeHead(200, { 'Content-Type': 'text/plain' });
-                res.end(responseText);
-            })
-            .catch(() => {
-                responseText += 'Cannot load the database';
-                res.writeHead(500, { 'Content-Type': 'text/plain' });
-                res.end(responseText);
-            });
-    }
+    countStudents(process.argv[2])
+      .then((studentInfo) => {
+        responseText += studentInfo;
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end(responseText);
+      })
+      .catch(() => {
+        responseText += 'Cannot load the database';
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end(responseText);
+      });
+  }
 });
 
 app.listen(1245);
