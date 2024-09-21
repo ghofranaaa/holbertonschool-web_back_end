673630152ebaeb00c69ff8382c2766dfb752e259
ghofranaaa <smailikhaled841@gmail.com>
2024-09-21 19:37:38 +0100

    corrected

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -4,22 +4,22 @@ const countStudents = require('./3-read_file_async');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send('Hello Holberton School!');
+  res.send('Hello Holberton School!');
 });
 
 app.get('/students', (req, res) => {
-    let responseText = 'This is the list of our students\n';
+  let responseText = 'This is the list of our students\n';
 
-	countStudents(process.argv[2])
-		.then((data) => {
-            responseText += data;
-			res.send(responseText);
-		})
-		.catch(() => {
-			res.status(500).send('Cannot load the database');
-		});
+  countStudents(process.argv[2])
+    .then((data) => {
+      responseText += data;
+      res.send(responseText);
+    })
+    .catch(() => {
+      responseText += 'Cannot load the database';
+      res.status(500).send(responseText);
+    });
 });
-
 
 app.listen(1245);
 
